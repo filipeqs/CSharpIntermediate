8e16cf6ceb3ec68a302c45aee43d413713f964ad
Filipe Silva <filipeqs@outlook.com>
2021-07-18 13:58:10 -0400

    classes

--- CSharpIntermediate/Program.cs
+++ CSharpIntermediate/Program.cs
@@ -2,11 +2,33 @@
 
 namespace CSharpIntermediate
 {
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine($"Hi {to}, I am {Name}");
+        }
+
+        public static Person Parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+
+            return person;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var filipe = new Person() { Name = "Filipe" };
+            filipe.Introduce("Mosh");
+
+            var john = Person.Parse("John");
+            john.Introduce(filipe.Name);
         }
     }
 }
