0a85c866e8eb4052ff6f5a70c262a7633757ed20
Filipe Silva <filipeqs@outlook.com>
2021-07-18 14:42:18 -0400

    constructors

---
+++ CSharpIntermediate/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CSharpIntermediate
+{
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine($"Hi {to}, I am {Name}");
+        }
+
+        public static Person Parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+
+            return person;
+        }
+    }
+}
--- CSharpIntermediate/Program.cs
+++ CSharpIntermediate/Program.cs
@@ -1,34 +1,10 @@
-﻿using System;
-
-namespace CSharpIntermediate
+﻿namespace CSharpIntermediate
 {
-    public class Person
-    {
-        public string Name;
-
-        public void Introduce(string to)
-        {
-            Console.WriteLine($"Hi {to}, I am {Name}");
-        }
-
-        public static Person Parse(string str)
-        {
-            var person = new Person();
-            person.Name = str;
-
-            return person;
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            var filipe = new Person() { Name = "Filipe" };
-            filipe.Introduce("Mosh");
-
-            var john = Person.Parse("John");
-            john.Introduce(filipe.Name);
+            var customer = new Customer(1, "John");
         }
     }
 }
