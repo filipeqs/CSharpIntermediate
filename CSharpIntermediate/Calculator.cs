1528dcecc344e37c976fd3ac97a4ff621b43b951
Filipe Silva <filipeqs@outlook.com>
2021-07-18 15:27:59 -0400

    overload, params and out

---
+++ CSharpIntermediate/Calculator.cs
@@ -0,0 +1,14 @@
+﻿namespace CSharpIntermediate
+{
+    public class Calculator
+    {
+        public static int Add(params int[] numbers)
+        {
+            var sum = 0;
+            foreach (var number in numbers)
+                sum += number;
+
+            return sum;
+        }
+    }
+}
--- CSharpIntermediate/Program.cs
+++ CSharpIntermediate/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace CSharpIntermediate
+﻿using System;
+using System.Linq;
+
+namespace CSharpIntermediate
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
-            var customer = new Customer(1, "John");
+            //var number = int.Parse("abc");
+
+            int number;
+            var result = int.TryParse("abc", out number);
+            if (result)
+                Console.WriteLine(number);
+            else
+                Console.WriteLine("Convertion Failed");
+        }
+
+        static void UsePoints()
+        {
+            try
+            {
+                var point = new Point(10, 20);
+                point.Move(null);
+                Console.WriteLine($"Point is at ({point.X}, {point.Y})");
+
+                point.Move(100, 200);
+                Console.WriteLine($"Point is at ({point.X}, {point.Y})");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        static void UseParama()
+        {
+            Console.WriteLine(Calculator.Add(1, 2));
+            Console.WriteLine(Calculator.Add(1, 2, 3, 4));
         }
     }
 }
