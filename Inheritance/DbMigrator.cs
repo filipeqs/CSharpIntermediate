7973d544b76d1b246a7af5e96de87866a6766ff7
Filipe Silva <filipeqs@outlook.com>
2021-07-20 08:01:39 -0400

    composition

---
+++ Inheritance/DbMigrator.cs
@@ -0,0 +1,21 @@
+﻿namespace Inheritance
+{
+    partial class Program
+    {
+        public class DbMigrator
+        {
+            public Logger Logger { get; }
+
+            public DbMigrator(Logger logger)
+            {
+                Logger = logger;
+            }
+
+            public void Migrate()
+            {
+                Logger.Log("We are migrating...");
+            }
+
+        }
+    }
+}
--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -2,13 +2,29 @@
 
 namespace Inheritance
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
+        {
+            UseComposition();
+        }
+
+        static void UseInheritance()
         {
             var text = new Text();
             text.Width = 100;
             text.Copy();
+        }
+
+        static void UseComposition()
+        {
+            var dbMigrator = new DbMigrator(new Logger());
+
+            var logger = new Logger();
+            var installer = new Installer(logger);
+
+            dbMigrator.Migrate();
+            installer.Install();
         }
     }
 }
