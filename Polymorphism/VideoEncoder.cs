6124962a67463bfdc3f0ca6fa199e8f705a7f418
Filipe Silva <filipeqs@outlook.com>
2021-07-22 07:56:29 -0400

    polymorphism

---
+++ Polymorphism/VideoEncoder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Polymorphism
+{
+    public class VideoEncoder
+    {
+        private readonly IList<INotificationChannel> NotificationChannels;
+        public VideoEncoder()
+        {
+            NotificationChannels = new List<INotificationChannel>();
+        }
+
+        public void Encode(Video video)
+        {
+            // Video encoding logic
+            foreach (var chanell in NotificationChannels)
+                chanell.Send(new Message());
+        }
+
+        public void RegisterNotificationChannel(INotificationChannel channel)
+        {
+            NotificationChannels.Add(channel);
+        }
+    }
+}
