1172865815ddb294736246bcee50aec07a30dd12
Filipe Silva <filipeqs@outlook.com>
2021-07-22 07:36:57 -0400

    extensibility

---
+++ Extensibility/DbMigrator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Extensibility
+{
+    public class DbMigrator
+    {
+        private readonly ILogger Logger;
+
+        public DbMigrator(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+
+        public void Migrate()
+        {
+            Logger.LogInfo($"Migration started at {DateTime.Now}");
+
+            // Details of migrating the database
+
+            Logger.LogInfo($"Migration ended at {DateTime.Now}");
+        }
+    }
+}
---
+++ Extensibility/FileLogger.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+
+namespace Extensibility
+{
+    partial class Program
+    {
+        public class FileLogger : ILogger
+        {
+            private readonly string Path;
+
+            public FileLogger(string path)
+            {
+                Path = path;
+            }
+
+            public void LogError(string message)
+            {
+                Log(message, "ERROR");
+            }
+
+            public void LogInfo(string message)
+            {
+                Log(message, "INFO");
+            }
+
+            private void Log(string message, string messageType)
+            {
+                using var streamWriter = new StreamWriter(Path, true);
+                streamWriter.WriteLine($"{messageType}: {message}");
+            }
+        }
+    }
+}
